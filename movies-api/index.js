2ae9dc970110c6c07c8fcee56469025f62491eb0
Jack Duggan <jackduggan01@hotmail.com>
2022-11-28 11:53:56 +0000

    added routing for genres | exercise 1

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 //updating index.js to use the new movies routing script
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
+app.use('/api/genres', genresRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
