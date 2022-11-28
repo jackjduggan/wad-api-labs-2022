fe26f7ca7a159716d48b122b05cfb4cbf0320376
Jack Duggan <jackduggan01@hotmail.com>
2022-11-28 15:12:37 +0000

    Mongo lab Exercises

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,5 +1,7 @@
 import userModel from '../api/users/userModel';
+import genresModel from '../api/genres/genresModel';
 import users from './users';
+import genres from './genres';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,7 +18,19 @@ async function loadUsers() {
   }
 }
 
+async function loadGenres() {
+    console.log('load genre Data');
+    try {
+      await genresModel.deleteMany();
+      await genresModel.collection.insertMany(genres);
+      console.info(`${genres.length} genres were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load genre Data: ${err}`);
+    }
+  }
+
 // eslint-disable-next-line no-undef
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres();
 }
