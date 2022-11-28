ba8b0c21080413fe8380cf66cdeab39c64e83df2
Jack Duggan <jackduggan01@hotmail.com>
2022-11-28 12:34:32 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import express from 'express';
 //updating index.js to use the new movies routing script
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -15,8 +16,10 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/api/movies', moviesRouter);
-app.use('/api/genres', genresRouter);
+
+app.use('/api/movies', moviesRouter); //movies router
+app.use('/api/genres', genresRouter); //genres router
+app.use('/api/users', usersRouter); //users router
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
